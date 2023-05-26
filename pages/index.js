0e298ddc7a37879af7361810d09d0fd3316d5eea
Bryan Magistrado <u1854321@unimail.hud.ac.uk>
2023-05-26 01:18:55 +0100

    BM - Implemented Chat Engine in Chat Component
    -Implemented the chat engine API inside of the chat component, will need to do some testing and readjustments to certain features such as the DM feature and the color scheme.

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,13 @@ import React, { useContext } from "react";
 
 import { Context } from '../context'
 
-import { useRouter } from "next/dist/client/router";
+import { useRouter } from "next/router";
 
 import axios from "axios";
+
+/**
+ * * Private key: 89172c7e-ee38-4e2e-8fa8-7def93d9ac4e
+ */
 
 export default function Auth() {
   const {
@@ -12,12 +16,27 @@ export default function Auth() {
     setUsername,
     secret,
     useSecret,
-  } = useContext(Context)
+  } = useContext(Context);
+
+  const router = useRouter();
+
+  function onSubmit(e) {
+    e.preventDefault()
+
+    if (username.length === 0 || secret.length === 0) return
+
+    axios.put(
+      'https://api.chatengine.io/users/',
+      {username: username, secret: secret},
+      {headers: {"Private-key": '89172c7e-ee38-4e2e-8fa8-7def93d9ac4e'}}
+    )
+    .then(r => router.push('/chats'))
+  }
 
   return (
     <div className="background">
       <div className="auth-container">
-        <form className="auth-form" onSubmit={e => e.preventDefault()}>
+        <form className="auth-form" onSubmit={e => onSubmit(e)}>
           <div className="auth-title">Next Chat App</div>
 
           <div className="input-container">
